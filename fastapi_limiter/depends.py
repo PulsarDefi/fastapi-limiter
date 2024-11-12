80a7502098e9285e5bfdbe1fd6a19c6debc4a8b2
claudiofcguerra <c.guerra@campus.fct.unl.pt>
2024-11-12 16:37:56 +0000

    Remove dep_index.

--- fastapi_limiter/depends.py
+++ fastapi_limiter/depends.py
@@ -27,35 +27,29 @@ class RateLimiter:
 
     async def _check(self, key):
         redis = FastAPILimiter.redis
-        pexpire = await redis.evalsha(
-            FastAPILimiter.lua_sha, 1, key, str(self.times), str(self.milliseconds)
-        )
+        pexpire = await redis.evalsha(FastAPILimiter.lua_sha, 1, key, str(self.times), str(self.milliseconds))
         return pexpire
 
     async def __call__(self, request: Request, response: Response):
         if not FastAPILimiter.redis:
             raise Exception("You must call FastAPILimiter.init in startup event of fastapi!")
-        route_index = 0
-        dep_index = 0
-        for i, route in enumerate(request.app.routes):
-            if route.path == request.scope["path"] and request.method in route.methods:
-                route_index = i
-                for j, dependency in enumerate(route.dependencies):
+        index = 0
+        for route in request.app.routes:
+            if route.path == request.scope["path"]:
+                for idx, dependency in enumerate(route.dependencies):
                     if self is dependency.dependency:
-                        dep_index = j
+                        index = idx
                         break
 
         # moved here because constructor run before app startup
         identifier = self.identifier or FastAPILimiter.identifier
         callback = self.callback or FastAPILimiter.http_callback
         rate_key = await identifier(request)
-        key = f"{FastAPILimiter.prefix}:{rate_key}:{route_index}:{dep_index}"
+        key = f"{FastAPILimiter.prefix}:{rate_key}:{index}"
         try:
             pexpire = await self._check(key)
         except pyredis.exceptions.NoScriptError:
-            FastAPILimiter.lua_sha = await FastAPILimiter.redis.script_load(
-                FastAPILimiter.lua_script
-            )
+            FastAPILimiter.lua_sha = await FastAPILimiter.redis.script_load(FastAPILimiter.lua_script)
             pexpire = await self._check(key)
         if pexpire != 0:
             return await callback(request, response, pexpire)
